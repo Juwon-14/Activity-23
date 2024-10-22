3cafb5f5d2060816ceac7120637d4d05f61c7620
Juwon-14 <wndnjs9127@gmail.com>
2024-10-21 21:47:09 -0700

    Update main.cpp

--- Activity-23/main.cpp
+++ Activity-23/main.cpp
@@ -2,13 +2,47 @@
 //  main.cpp
 //  Activity-23
 //
-//  Created by 홍주원 on 10/21/24.
+//  Created by Juwon Hong on 10/21/24.
 //
 
 #include <iostream>
+#include <string>
+#include <cctype>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+bool CustomerNumberFormat(const string& customerNumber) {
+    if (customerNumber.size() != 6) {
+        return false;
+    }
+
+    // Check if the first two characters are alphabetic
+    if (!isalpha(customerNumber[0]) || !isalpha(customerNumber[1])) {
+        return false;
+    }
+
+    // Check if the last four characters are numeric
+    for (int i = 2; i < 6; ++i) {
+        if (!isdigit(customerNumber[i])) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    string input;
+    
+    cout << "Enter a customer number: ";
+    cin >> input;
+    
+    if (CustomerNumberFormat(input)) {
+        cout << "Valid customer number." << endl;
+    } else {
+        cout << "Invalid customer number." << endl;
+    }
+    
     return 0;
 }
+
