6ab83b4afc4f7e324051d2c573f298ace153d550
Juwon-14 <wndnjs9127@gmail.com>
2024-10-23 11:54:52 -0700

    Add files via upload

---
+++ Activity-23/customer.h
@@ -0,0 +1,17 @@
+//
+//  customer.h
+//  Activity-23
+//
+//  Created by Juwon Hong on 10/23/24.
+//
+
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <string>
+using namespace std;
+
+// Function to validate the customer number format
+bool CustomerNumberFormat(const string& customerNumber);
+
+#endif
